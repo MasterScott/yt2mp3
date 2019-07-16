e517115a3d15463ff6d91aad80f715a861caebf0
Michał Wróblewski <20270277+DeBos99@users.noreply.github.com>
2019-07-16 10:59:38 +0100

    Fix bug

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,14 @@
 
 All notable changes to this project will be documented in this file.
 
+## [0.0.3] - 2019-07-16
+
+### Fixed
+
+##### [main.pl](main.pl)
+
+- Regex matching video ID.
+
 ## [0.0.2] - 2019-07-15
 
 ### Added
--- main.pl
+++ main.pl
@@ -2,6 +2,6 @@ use strict;
 use warnings;
 
 my @songs;
-while(<>){push(@songs,"https://www.youtube.com/watch?$&") if(/v=\w+/)};
+while(<>){push(@songs,"https://www.youtube.com/watch?$&") if(/v=[\w_-]+/)};
 system(($^O=~/^ms/i ?'./':'').'youtube-dl','--geo-bypass','-x','--audio-format','mp3','--audio-quality','0','-o','%(title)s.%(ext)s',@songs);
 rename($_,$_=~s/^\s+|(\([^\)]*\))|[^ -~]+|\s+$//gr) foreach(glob('*.mp3'));
